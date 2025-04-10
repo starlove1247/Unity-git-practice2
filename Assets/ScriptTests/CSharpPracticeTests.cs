f6a04e2d0c9135d242c47faa053d78cb66faf428
rStar <starlove1247@gmail.com>
2025-04-10 15:02:43 +0800

    :sparkles: 取得本地化文字

--- Assets/ScriptTests/CSharpPracticeTests.cs
+++ Assets/ScriptTests/CSharpPracticeTests.cs
@@ -3,10 +3,10 @@ using NUnit.Framework;
 public class CSharpPracticeTests
 {
     [Test(Description = "兩個數字相加的驗證")]
-    [TestCase(2 , 2 , 4)]
-    [TestCase(3 , 3 , 6)]
-    [TestCase(4 , 4 , 8)]
-    [TestCase(-4 , 6, 2 , Description = "負數加正數")]
+    [TestCase(2 ,  2 , 4)]
+    [TestCase(3 ,  3 , 6)]
+    [TestCase(4 ,  4 , 8)]
+    [TestCase(-4 , 6 , 2 , Description = "負數加正數")]
     public void Add_Two_Number(int a , int b , int expected)
     {
         // arrange
@@ -14,10 +14,23 @@ public class CSharpPracticeTests
 
         // act
         int result = csharpPractice.Add(a , b);
-    
+
         // assert
         Assert.AreEqual(expected , result);
     }
 
-    
+    [Test(Description = "取得本地化文字")]
+    [TestCase("Apple" , "蘋果")]
+    [TestCase("Banana" , "香蕉")]
+    public void GetLocalizationText(string key , string expected)
+    {
+        // arrange
+        var csharpPractice = new CsharpPractice();
+
+        // act
+        var localizationText = csharpPractice.GetLocalizationText(key);
+
+        // assert
+        Assert.AreEqual(expected , localizationText);
+    }
 }
--- Assets/Scripts/CsharpPractice.cs
+++ Assets/Scripts/CsharpPractice.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CsharpPractice
@@ -5,5 +6,30 @@ public class CsharpPractice
     public int Add(int a , int b)
     {
         return a + b;
+    }
+
+    /// <summary>
+    /// Key , Value
+    /// 英文 , 中文
+    /// </summary>
+    private Dictionary<string , string> localizationTexts = new Dictionary<string , string>()
+    {
+        { "Apple" , "蘋果" } , { "Banana" , "香蕉" } ,
+    };
+
+    public string GetLocalizationText(string key)
+    {
+        return localizationTexts[key];
+        // if (key == "Apple")
+        // {
+        //     return "蘋果";
+        // }
+        //
+        // if (key == "Banana")
+        // {
+        //     return "香蕉";
+        // }
+        //
+        // return "";
     }
 }
